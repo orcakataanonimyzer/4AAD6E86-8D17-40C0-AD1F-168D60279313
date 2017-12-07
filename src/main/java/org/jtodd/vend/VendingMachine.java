048b4fc11fe44e47485ce6b892aa50c53f164158
anonymous <anonymous>
2017-12-06 20:39:08 -0500

    Can recognize all valid coins

--- src/main/java/org/jtodd/vend/VendingMachine.java
+++ src/main/java/org/jtodd/vend/VendingMachine.java
@@ -3,10 +3,12 @@ package org.jtodd.vend;
 public class VendingMachine {
 
     public boolean accept(Coin coin) {
-        if (coin.mass < 3) {
-            return false;
-        } else {
+        if ((coin.diameter == 21.21 && coin.mass == 5.0) ||
+            (coin.diameter == 17.91 && coin.mass == 2.268) ||
+            (coin.diameter == 24.26 && coin.mass == 5.67)) {
             return true;
+        } else {
+            return false;
         }
     }
 }
--- src/test/java/org/jtodd/vend/TestVendingMachine.java
+++ src/test/java/org/jtodd/vend/TestVendingMachine.java
@@ -9,13 +9,21 @@ public class TestVendingMachine {
     public void testAcceptsValidCoin() {
         VendingMachine machine = new VendingMachine();
         Coin nickel = new Coin(21.21, 5);
+        Coin dime = new Coin(17.91, 2.268);
+        Coin quarter = new Coin(24.26, 5.67);
         Assert.assertTrue("Vending machine should have accepted valid coin", machine.accept(nickel));
+        Assert.assertTrue("Vending machine should have accepted valid coin", machine.accept(dime));
+        Assert.assertTrue("Vending machine should have accepted valid coin", machine.accept(quarter));
     }
 
     @Test
     public void testDoesNotAcceptInvalidCoin() {
         VendingMachine machine = new VendingMachine();
         Coin penny = new Coin(19.05, 2.5);
+        Coin halfDollar = new Coin(30.61, 11.34);
+        Coin dollar = new Coin(26.49, 8.1);
         Assert.assertFalse("Vending machine should not have accepted invalid coin", machine.accept(penny));
+        Assert.assertFalse("Vending machine should not have accepted invalid coin", machine.accept(halfDollar));
+        Assert.assertFalse("Vending machine should not have accepted invalid coin", machine.accept(dollar));
     }
 }
