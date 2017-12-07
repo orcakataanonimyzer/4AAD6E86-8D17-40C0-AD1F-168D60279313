724a7f5c7affe700c799121ab8d0a90da2b52f13
anonymous <anonymous>
2017-12-06 21:56:13 -0500

    Machine gives correct display

--- src/test/java/org/jtodd/vend/TestVendingMachine.java
+++ src/test/java/org/jtodd/vend/TestVendingMachine.java
@@ -45,4 +45,24 @@ public class TestVendingMachine {
         machine.accept(new Coin(Currency.QUARTER));
         Assert.assertEquals("$0.30", machine.getDisplay());
     }
+
+    @Test
+    public void testDisplayInsertCoinWhenInvalidCoinDeposited() {
+        machine.accept(new Coin(0.5, 0.5));
+        Assert.assertEquals("INSERT COIN", machine.getDisplay());
+    }
+
+    @Test
+    public void testDisplayInsertCoinWhenValidCoinThenInvalidCoinDeposited() {
+        machine.accept(new Coin(Currency.NICKEL));
+        machine.accept(new Coin(0.5, 0.5));
+        Assert.assertEquals("$0.05", machine.getDisplay());
+    }
+
+    @Test
+    public void testDisplaysCorrectDepositedAmountWhenInvalidCoinThenValidCoinDeposited() {
+        machine.accept(new Coin(0.5, 0.5));
+        machine.accept(new Coin(Currency.NICKEL));
+        Assert.assertEquals("$0.05", machine.getDisplay());
+    }
 }
